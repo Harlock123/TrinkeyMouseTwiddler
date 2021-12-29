0e54048384bd53b2731e074697611bb1a8369f9f
Lonnie Watson <Harlock123@gmail.com>
2021-12-29 12:06:28 -0500

    Added an alternate Code file for a different blink pattern
    Added an alternate Code file for a different blink pattern..
    Rename the code.py file and rename this one to code.py to activate it...

---
+++ AlternateBlinky.py
@@ -0,0 +1,89 @@
+import usb_hid
+import time
+import board
+import neopixel
+from adafruit_hid.mouse import Mouse
+
+# pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+p1 = neopixel.NeoPixel(board.NEOPIXEL, 4, brightness=0.1, auto_write=False)
+m = Mouse(usb_hid.devices)
+
+modulo = 0
+
+while True:
+
+#    if modulo == 0:
+#        p1[0] = (255, 0, 0)
+#        p1[1] = (0, 255, 0)
+#        p1[2] = (0, 0, 255)
+#        p1[3] = (0, 0, 0)
+
+#    if modulo == 3:
+#        p1[3] = (255, 0, 0)
+#        p1[0] = (0, 255, 0)
+#        p1[1] = (0, 0, 255)
+#        p1[2] = (0, 0, 0)
+
+#    if modulo == 2:
+#        p1[2] = (255, 0, 0)
+#        p1[3] = (0, 255, 0)
+#        p1[0] = (0, 0, 255)
+#        p1[1] = (0, 0, 0)
+
+#    if modulo == 1:
+#        p1[1] = (255, 0, 0)
+#        p1[2] = (0, 255, 0)
+#        p1[3] = (0, 0, 255)
+#        p1[0] = (0, 0, 0)
+
+#    p1.show()
+
+#    modulo = modulo + 1
+
+#    if modulo == 5:
+#        modulo = 0
+
+    time.sleep(30)
+
+    m.move(2, 0)
+    p1[0] = (255, 0, 0)
+    p1[1] = (0, 255, 0)
+    p1[2] = (0, 0, 255)
+    p1[3] = (0, 0, 0)
+    p1.show()
+    time.sleep(0.1)
+    m.move(0, 2)
+    p1[3] = (255, 0, 0)
+    p1[0] = (0, 255, 0)
+    p1[1] = (0, 0, 255)
+    p1[2] = (0, 0, 0)
+    p1.show()
+    time.sleep(0.1)
+    m.move(-2, 0)
+    p1[2] = (255, 0, 0)
+    p1[3] = (0, 255, 0)
+    p1[0] = (0, 0, 255)
+    p1[1] = (0, 0, 0)
+    p1.show()
+    time.sleep(0.1)
+    m.move(0, -2)
+    p1[1] = (255, 0, 0)
+    p1[2] = (0, 255, 0)
+    p1[3] = (0, 0, 255)
+    p1[0] = (0, 0, 0)
+    p1.show()
+    time.sleep(0.1)
+    p1[1] = (0, 0, 0)
+    p1[2] = (0, 0, 0)
+    p1[3] = (0, 0, 0)
+    p1[0] = (0, 0, 0)
+    p1.show()
+    time.sleep(0.1)
+
+    
+
+    # pixels.fill((255, 0, 0))
+    # time.sleep(0.5)
+    # pixels.fill((0, 0, 0))
+    # time.sleep(0.5)
